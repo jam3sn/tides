7b0a5a8ed8d854356e1c232140c0d43133fb7025
James Newman <j@mesnewman.dev>
2022-04-26 18:33:01 +0100

    place validation

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,10 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { place } = request.query;
+
+  if (place === undefined) {
+    response.status(400).send('Place parameter required. E.g. /api?place=London,UK');
+  }
+  
   response.status(200).send(`Place: ${place}`);
 };
